dfbd3c877ca1736495029cec1212c1ad6310849a
Strencher <46447572+Strencher@users.noreply.github.com>
2021-02-27 20:12:32 +0100

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 import { Plugin } from '@vizality/entities';
+import { patch, unpatch } from '@vizality/patcher';
+import { findInReactTree } from '@vizality/util/react';
 import { getModule } from '@vizality/webpack';
+import TypingIcon from './components/typingIcon';
+import React from 'react';
 
+const ChannelTextAreContainer = getModule(m => m?.type?.render?.displayName === 'ChannelTextAreaContainer')?.type
 const typingModule = getModule('startTyping', 'stopTyping');
 
 export default class SilentTyping extends Plugin {
-  start () {
-    this._startTyping = typingModule.startTyping;
-    typingModule.startTyping = () => void 0;
-  }
+   unpatches = [];
+   
+   start() {
+      this.unpatches.push(
+         this.patchChannelTextArea(),
+         this.patchTypingModule()
+      );
 
-  stop () {
-    typingModule.startTyping = this._startTyping;
-  }
+      this.injectStyles('style.scss');
+   }
+      
+   patchTypingModule() {
+      let oldStartTyping = typingModule.startTyping;
+      
+      typingModule.startTyping = channelId => {
+         if (!channelId) return false;
+         if (this.settings.get('excludeChannels', []).indexOf(channelId) < 0) return false;
+         oldStartTyping(channelId);
+      };
+
+      return () => typingModule.startTyping = oldStartTyping;
+   }
+
+   removeChannel = channelId => {
+      const settings = this.settings.get('excludeChannels', []);
+      if (settings.indexOf(channelId) < 0) return;
+      settings.splice(settings.indexOf(channelId), 1);
+      this.settings.set('excludeChannels', settings);
+   }
+
+   addChannel = channelId => {
+      const settings = this.settings.get('excludeChannels', []);
+      if (settings.indexOf(channelId) > -1) return;
+      this.settings.set('excludeChannels', settings.concat(channelId));
+   }
+
+   patchChannelTextArea() {
+      patch('silent-typing-textarea', ChannelTextAreContainer, 'render', ([{channel, textValue}], res) => {
+         const tree = findInReactTree(res, e => e?.className?.indexOf('buttons') > -1);
+         if (!Array.isArray(tree?.children)) return res;
+
+         
+         tree.children.unshift(
+            <TypingIcon
+               settings={this.settings}
+               addChannel={this.addChannel}
+               removeChannel={this.removeChannel}
+               channel={channel}
+               textValue={textValue}
+               typingModule={typingModule}
+            />
+         );
+
+         return res;
+      });
+
+      return () => unpatch('silent-typing-textarea');
+   }
+
+   stop() {
+      for (const unpatch of this.unpatches) unpatch();
+   }
 };
